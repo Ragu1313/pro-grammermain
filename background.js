0cf1eb604485c5253aad029787641e5cee9a12c2
Ragu1313 <raguramesh252004@gmail.com>
2025-03-19 12:25:15 +0530

    1.1 complete

---
+++ background.js
@@ -0,0 +1,27 @@
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "openAutoCorrect",
+        title: "Pro-Grammer",
+        contexts: ["selection", "editable"]
+    });
+});
+
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "openAutoCorrect") {
+        chrome.storage.local.set({ selectedText: info.selectionText });
+        chrome.windows.create({
+            url: "popup.html",
+            type: "popup",
+            width: 400,
+            height: 300,
+            
+        });
+        
+
+    }
+});
+
+
+
